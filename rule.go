27d2494061e594d68b1ff3a78060c4fb4bda0bac
Connor Snitker <csnitker@godaddy.com>
2024-02-23 10:04:08 -0600

    Added parsing for nexus category

--- rule.go
+++ rule.go
@@ -191,6 +191,9 @@ var (
 		"registrant contact e mail":              "registrant_email",
 		"registrant abuse contact email":         "registrant_email",
 		"registrar authorised registrar":         "registrar_id",
+		"registrant attention":                   "registrant_email",
+		"registrant nexus category":              "registrant_extended_nexus_category",
+		"registrant application purpose":         "registrant_extended_application_purpose",
 		"reseller":                               "reseller_name",
 	}
 )
